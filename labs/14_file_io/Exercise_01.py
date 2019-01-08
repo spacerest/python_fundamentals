e7a3a6361194d4dea2a3e2060a4ce20065814ae6
Caden MacKenzie <cadenmackenzie@gmail.com>
2019-01-08 09:40:18 -0700

    initial commit with labs up to intermediate

---
+++ labs/14_file_io/Exercise_01.py
@@ -0,0 +1,10 @@
+'''
+Write a script that generates an exception. Handle this exception with a try/except.
+For example:
+
+_list = ["hello world!"]
+print(_list[1])
+
+This raises and exception that needs to be handled.
+
+'''
