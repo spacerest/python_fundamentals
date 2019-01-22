8735a10bb5982926dead24b5f6c38004b2265516
martin-martin <breuss.martin@gmail.com>
2019-01-22 09:51:16 +0700

    fix typo

--- labs/03_more_datatypes/2_lists/04_10_unique.py
+++ labs/03_more_datatypes/2_lists/04_10_unique.py
@@ -1,7 +1,7 @@
 '''
 Write a script that creates a list of all unique values in a list. For example:
 
-_list = [1, 2, 6, 55, 2,, 'hi', 4, 6, 1, 13]
+list_ = [1, 2, 6, 55, 2, 'hi', 4, 6, 1, 13]
 unique_list = [55, 'hi', 4, 13]
 
 
