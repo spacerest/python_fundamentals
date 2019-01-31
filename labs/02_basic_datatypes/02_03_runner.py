128547180b110b7fa710894b754d13881e1b8607
Martin Breuss <martin-martin@users.noreply.github.com>
2019-01-31 10:27:49 +0700

    fix typo

--- labs/02_basic_datatypes/02_03_runner.py
+++ labs/02_basic_datatypes/02_03_runner.py
@@ -1,6 +1,6 @@
 '''
 
-If a runner runs 10 miles in 30 minites and 30 seconds,
+If a runner runs 10 miles in 30 minutes and 30 seconds,
 What is his/her average speed in kilometers per hour? (Tip: 1 mile = 1.6 km)
 
 '''
