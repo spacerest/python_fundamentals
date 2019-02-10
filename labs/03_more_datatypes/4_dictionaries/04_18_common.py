a2559241a173d7e0ba741a8d44f5da6351605420
Martin Breuss <martin-martin@users.noreply.github.com>
2019-02-10 11:15:31 +0700

    fix variable naming

--- labs/03_more_datatypes/4_dictionaries/04_18_common.py
+++ labs/03_more_datatypes/4_dictionaries/04_18_common.py
@@ -2,10 +2,9 @@
 Write a script that takes the following two dictionaries and creates a new dictionary by combining
 the common keys and adding the values of duplicate keys together. For example:
 
-_dict1 = {"a": 1, "b": 2, "c": 3}
-_dict2 = {"a": 2, "c": 4 , "d": 2}
+dict_1 = {"a": 1, "b": 2, "c": 3}
+dict_2 = {"a": 2, "c": 4 , "d": 2}
 
 result = {"a": 3, "b": 2, "c": 7 , "d": 2}
-
 
 '''
