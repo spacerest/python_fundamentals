94ba69c6db0b4ab779d6151841663ec0d7ce9f57
Sadie Parker <sadiemparker@gmail.com>
2019-07-10 18:21:32 +0930

    update wording and remove ambiguous task

--- 05_functions/05_01_tasks.py
+++ 05_functions/05_01_tasks.py
@@ -4,13 +4,12 @@ Write a script that completes the following tasks.
 
 '''
 
+# define a function that determines whether the number is divisible by 4 or 7 and returns a boolean
 
-# takes in a number from the user between 1 and 1,000,000,000
+# define a function that determines whether a number is divisible by both 4 and 7 and returns a boolean
 
-# calls a function that determines whether the number is divisible by both 4 and 7
+# take in a number from the user between 1 and 1,000,000,000
 
-# calls a function that determines whether the number is divisible by 4 or 7
+# call your functions, passing in the user input as the arguments, and set their output equal to new variables 
 
-# calls a function that determines whether the number is divisible by 4 or 7 exclusively
-
-# print the results
+# print your new variables to display the results
